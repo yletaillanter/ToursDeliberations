caf2c285fb2c4f30d301873008d671c14c77938b
yletaillanter <y.letaillanter@gmail.com>
2020-12-09 10:26:34 +0100

    adding new Fragment + navigation in Compose

--- app/src/main/java/com/ylt/toursdeliberations/ui/main/composable/DeliberationsCard.kt
+++ app/src/main/java/com/ylt/toursdeliberations/ui/main/composable/DeliberationsCard.kt
@@ -1,6 +1,5 @@
 package com.ylt.toursdeliberations.ui.main.composable
 
-import android.widget.Toast
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
@@ -16,36 +15,37 @@ import androidx.compose.ui.platform.ContextAmbient
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import com.ylt.toursdeliberations.R
 import com.ylt.toursdeliberations.model.Deliberations
 import com.ylt.toursdeliberations.model.Record
 import com.ylt.toursdeliberations.ui.main.MainViewModel
-import timber.log.Timber
 
 @Composable
-fun DeliberationsCardView(delib: Deliberations){
-    val context = ContextAmbient.current
-
+fun DeliberationsCardView(delib: Deliberations, navController: NavController){
+    // arg = delib.delibId
     Column(
-        Modifier.padding(8.dp).clickable(onClick = { Toast.makeText(context,delib.toString(), Toast.LENGTH_SHORT).show()})
+        Modifier.padding(8.dp).clickable(onClick = { navController.navigate(R.id.to_detailFragment)})
     ) {
-        Text(text = delib.delibObjet, color = Color.Blue, fontSize = 18.sp)
+        Text(text = delib.delibObjet, color = Color.Blue, fontSize = 16.sp)
         Text(text = delib.collNom, fontStyle = FontStyle.Italic)
     }
 }
 
 @Composable
-fun DeliberationsListView(viewModel:MainViewModel) {
+fun DeliberationsListView(viewModel:MainViewModel, navController: NavController) {
     val records: List<Record> by  viewModel.deliberationsList.observeAsState(listOf())
 
     LazyColumnFor(records) { record ->
-        DeliberationsCardView(record.deliberation)
+        DeliberationsCardView(record.deliberation, navController)
         Divider()
     }
 }
 
+/*
 @Preview
 @Composable
 fun DeliberationsCardPreview() {
-    DeliberationsCardView(Deliberations(delibObjet = "APPROBATION DU COMPTE ADMINISTRATIF DE L EXERCICE 2019 DU BUDGET PRINCIPAL ET AFFECTATION DU RESULTAT.", collNom = "Tours Métropole Val de Loire"))
+    //DeliberationsCardView(Deliberations(delibObjet = "APPROBATION DU COMPTE ADMINISTRATIF DE L EXERCICE 2019 DU BUDGET PRINCIPAL ET AFFECTATION DU RESULTAT.", collNom = "Tours Métropole Val de Loire"))
 }
+ */

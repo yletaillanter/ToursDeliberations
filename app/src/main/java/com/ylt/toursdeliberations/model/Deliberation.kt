11dcd75e4f11308164861a23a6a2a184d6945d8c
Yoann Le Taillanter <y.letaillanter@gmail.com>
2021-08-04 13:29:21 +0200

    Add content example

--- app/src/main/java/com/ylt/toursdeliberations/model/Deliberation.kt
+++ app/src/main/java/com/ylt/toursdeliberations/model/Deliberation.kt
@@ -26,3 +26,45 @@ class Deliberation(@SerializedName("delib_matiere_code") val delibMatiereCode: S
         return "Deliberations(delibMatiereCode='$delibMatiereCode', delibDate='$delibDate', delibObjet='$delibObjet', collNom='$collNom', delibId='$delibId', voteEffectifs=$voteEffectifs, collSiret='$collSiret', coteContre=$voteContre, delibMatiereNom='$delibMatiereNom', themes='$themes', TypeSeance='$typeSeance', voteReel=$voteReel, voteAbstention=$voteAbstention, votePour=$votePour, prefId='$prefId', prefDate='$prefDate')"
     }
 }
+
+/*
+{
+    "datasetid":"deliberations-tours-metropole-val-de-loire",
+    "recordid":"43e3d7d579076a06fc577a1044dfeb5c6c9c1942",
+    "fields": {
+        "type_seance":"Conseil Métropolitain",
+        "delib_matiere_code":"5.1.2",
+        "delib_objet":"DETERMINATION DU NOMBRE DE VICE-PRESIDENTS ET DES AUTRES MEMBRES DU BUREAU",
+        "coll_nom":"Tours Métropole Val de Loire",
+        "delib_id":"C_21_07_11_002",
+        "cr_url":{
+            "format":"pdf",
+            "filename":"1626905209_cr_30638.pdf",
+            "width":300,
+            "id":"e8783871774f15f0235f55c238d8e991",
+            "height":300,
+            "thumbnail":false
+        },
+    "vote_effectif":87,
+    "delib_url":{
+        "format":"pdf",
+        "filename":"1626905209_30637.pdf",
+        "width":300,
+        "id":"884eeb7e670e958b4360e3bbf06889e1",
+        "height":300,
+        "thumbnail":false
+    },
+        "coll_siret":24370075400035,
+        "delib_date":"2021-07-21",
+        "delib_matiere_nom":"Institutions et vie politique Election executif Fixation du nombre des adjoints",
+        "vote_pour":73,
+        "vote_reel":87,
+        "vote_abstention":7,
+        "vote_contre":5,
+        "pref_id":"SPREF0372",
+        "pref_date":"2021-07-21"
+    },
+    "record_timestamp":"2021-07-21T23:25:00+00:00"
+},
+
+ */
